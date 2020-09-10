b9c5493a102143a92f786b08a7ef4734d8539a16
BangKiHyun <rlrlvh@naver.com>
2020-09-10 16:38:57 +0900

    Update entity class

---
+++ src/main/java/jpabook/jpashop/domain/order/Order.java
@@ -0,0 +1,42 @@
+package jpabook.jpashop.domain.order;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.delivery.Delivery;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private String id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "order")
+    private List<OrderItem> orderItems = new ArrayList<>();
+
+    @OneToOne(fetch = LAZY)
+    @JoinColumn(name = "delivery_id")
+    private Delivery delivery;
+
+    private LocalDateTime orderDate; //주문 시간
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+}
