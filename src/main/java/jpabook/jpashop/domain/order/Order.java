64b98c8bc0e6131545f81909971e8c8657229edd
BangKiHyun <rlrlvh@naver.com>
2020-09-12 17:48:00 +0900

    refactor: 연관 관계 편의 메서드 추가, fetch 타입 설정

--- src/main/java/jpabook/jpashop/domain/order/Order.java
+++ src/main/java/jpabook/jpashop/domain/order/Order.java
@@ -28,10 +28,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = LAZY)
+    @OneToOne(fetch = LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -39,4 +39,20 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    //==연관관계 메서드==//
+    public void changeMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void changeDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 }
